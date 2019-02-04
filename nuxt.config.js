4978b21d4499b4b93481340e3061ee934bba4775
Michael Romanoff <zelcie@gmail.com>
2019-02-03 22:22:16 -0800

    add appolo

---
+++ nuxt.config.js
@@ -0,0 +1,134 @@
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/style/app.styl'],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: ['@/plugins/vuetify'],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/apollo'
+    // Doc: https://axios.nuxtjs.org/usage
+    //'@nuxtjs/axios'
+  ],
+
+  apollo: {
+    // networkInterfaces: {
+    //   default: '~/apollo/network-interfaces/default.js'
+    // },
+
+    //tokenName: 'yourApolloTokenName', // optional, default: apollo-token
+    //tokenExpires: 10, // optional, default: 7 (days)
+    //includeNodeModules: true, // optional, default: false (this includes graphql-tag for node_modules folder)
+    //authenticationType: 'Basic', // optional, default: 'Bearer'
+    // optional
+    errorHandler(error) {
+      console.log(
+        '%cError',
+        'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;',
+        error.message
+      )
+    },
+    // required
+    clientConfigs: {
+      default: {
+        // required
+        httpEndpoint: 'http://localhost:5000/graphql',
+        // optional
+        // See https://www.apollographql.com/docs/link/links/http.html#options
+        httpLinkOptions: {
+          credentials: 'same-origin'
+        },
+
+        ssr: true
+        // You can use `wss` for secure connection (recommended in production)
+        // Use `null` to disable subscriptions
+        //wsEndpoint: 'ws://localhost:5000', // optional
+        // LocalStorage token
+        //tokenName: 'apollo-token', // optional
+        // Enable Automatic Query persisting with Apollo Engine
+        //persisting: false, // Optional
+        // Use websockets for everything (no HTTP)
+        // You need to pass a `wsEndpoint` for this to work
+        //websocketsOnly: false // Optional
+      }
+      // test: {
+      //   httpEndpoint: 'http://localhost:5000',
+      //   wsEndpoint: 'ws://localhost:5000',
+      //   tokenName: 'apollo-token'
+      // },
+      // alternative: user path to config which returns exact same config options
+      //test2: '~/plugins/my-alternative-apollo-config.js'
+    }
+  },
+  /*
+  ** Axios module configuration
+  */
+  //axios: {
+  // See https://github.com/nuxt-community/axios-module#options
+  //},
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    }
+
+    /*
+    ** You can extend webpack config here
+    */
+    // extend(config, ctx) {
+    //   // Run ESLint on save
+    //   if (ctx.isDev && ctx.isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card class="card" v-for="vehicle in vehicles" :key="vehicle.id">
+        <v-img
+          :src="vehicle.vehicleDetails.image.hero.md"
+          :alt="`${vehicle.vehicleDetails.year } ${ vehicle.vehicleDetails.make } ${ vehicle.vehicleDetails.model} `"
+          aspect-ratio="1.7"
+        ></v-img>
+
+
+        <v-card-title class="headline">
+          {{ vehicle.vehicleDetails.year }} {{ vehicle.vehicleDetails.make }} {{ vehicle.vehicleDetails.model }}
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Bacon ipsum dolor sit amet salami jowl corned beef, andouille flank tongue ball tip kielbasa pastrami tri-tip meatloaf short loin beef biltong. Cow bresaola ground round strip steak fatback meatball shoulder leberkas pastrami sausage corned beef t-bone pork belly drumstick.
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-layout justify-center>
+            <v-btn
+              color="info"
+              dark
+              nuxt
+              :to="`/report/${vehicle.id}`"
+            >
+              Continue
+            </v-btn>
+          </v-layout>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import vehicles from '~/apollo/queries/vehicles.gql'
+
+export default {
+  apollo: {
+    vehicles: {
+      prefetch: true,
+      query: vehicles
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  margin-bottom: 1rem;
+  max-width: 500px;
+  padding-bottom: 1rem;
+}
+
+.card__image {
+  width: 200px;
+  height: auto;
+}
+</style>
